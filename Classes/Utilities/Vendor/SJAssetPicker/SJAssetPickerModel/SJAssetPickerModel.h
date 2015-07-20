d1ed906fcb6b768555df0a6ca64a218b187bafa9
shejunzhou <965678322@qq.com>
2015-07-20 23:43:19 +0800

    支持分别在各个文件夹下选择图片。

---
+++ Classes/Utilities/Vendor/SJAssetPicker/SJAssetPickerModel/SJAssetPickerModel.h
@@ -0,0 +1,30 @@
+//
+//  SJAssetPickerModel.h
+//  SJAssetPicker
+//
+//  Created by shejun.zhou on 15/7/20.
+//  Copyright (c) 2015年 shejun.zhou. All rights reserved.
+//
+
+/**
+ @file      SJAssetPickerModel.h
+ @abstract  读取相册model，同步已选择的相片对象
+ @author    shejun.zhou
+ @version   1.0 15/7/20 Creation
+ */
+#import <Foundation/Foundation.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+/**
+ @class     SJAssetPickerModel
+ @abstract  读取相册model
+ */
+@interface SJAssetPickerModel : NSObject
+
+@property (nonatomic, strong) NSMutableArray *selectedAssetsArray;
+@property (nonatomic, strong) NSMutableArray *selectedURLsArray;
+@property (nonatomic, strong) ALAssetsLibrary *library;
+
++ (SJAssetPickerModel *)shareManager;
+
+@end
